39df20c478b201b6c5cc34395ed7bc51cb37df00
thankyouthatscool <85291297+thankyouthatscool@users.noreply.github.com>
2021-07-25 09:09:27 +1000

    Re-formatted typeDefs.

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -36,9 +36,21 @@ const typeDefs = gql`
 
     signRequest: SignatureData!
 
-    checkAvailability(checkAvailabilityData: CHECK_AVAILABILITY_DATA): [String]!
+    checkAvailability(checkAvailabilityData: CHECK_AVAILABILITY_DATA): [String]
 
     paymentIntent(paymentIntentData: PAYMENT_INTENT_DATA): ClientSecret!
+  }
+
+  input CHECK_AVAILABILITY_DATA {
+    quantity: Int!
+    timeUnit: String!
+  }
+
+  input CONTACT_DATA {
+    comments: String
+    contact: String!
+    files: [Upload]
+    name: String!
   }
 
   input CREATE_NEW_ADMIN_DATA {
@@ -65,6 +77,117 @@ const typeDefs = gql`
     salePrice: Float
   }
 
+  input CREATE_NEW_SERVICE_DATA {
+    available: Boolean!
+    description: String!
+    duration: Int!
+    images: [String]
+    name: String!
+    options: [String]
+    price: Float!
+    salePrice: Float
+  }
+
+  input CUSTOMER_INPUT_DATA {
+    email: String!
+    firstName: String!
+    lastName: String!
+    phoneNumber: String!
+  }
+
+  input DELETE_BOOKING_DATA {
+    bookingId: ID!
+  }
+
+  input DELETE_PRODUCT_DATA {
+    productId: ID!
+  }
+
+  input DELETE_SERVICE_DATA {
+    serviceId: ID!
+  }
+
+  input EDIT_BOOKING_DATA {
+    bookingId: ID!
+    duration: Int!
+    serviceId: ID!
+    startTime: String!
+  }
+
+  input EDIT_PRODUCT_DATA {
+    available: Boolean!
+    description: String!
+    images: [String]
+    name: String!
+    options: [String]
+    price: Float!
+    productId: ID!
+    salePrice: Float
+  }
+
+  input EDIT_SERVICE_DATA {
+    available: Boolean!
+    description: String!
+    duration: Int!
+    images: [String]
+    name: String!
+    options: [String]
+    price: Float!
+    salePrice: Float
+    serviceId: ID!
+  }
+
+  input GET_PRODUCT_DATA {
+    productId: ID!
+  }
+
+  input GET_SERVICE_DATA {
+    serviceId: ID!
+  }
+
+  input LOGIN_DATA {
+    email: String!
+    password: String!
+  }
+
+  input PAYMENT_INTENT_DATA {
+    productId: [PRODUCT_ORDER]
+    serviceId: [ID]
+    discount: String
+    shipping: Boolean!
+  }
+
+  input PRODUCT_ORDER {
+    id: ID!
+    quantity: Int!
+  }
+
+  input SET_AVAILABILITY_DATA {
+    date: String!
+  }
+
+  input SHIPPING_ADDRESS_DATA {
+    streetName: String!
+    number: Int!
+    suburb: String!
+    postcode: Int!
+    state: String!
+  }
+
+  input SUBMIT_PURCHASE_DATA {
+    comments: String
+    customer: CUSTOMER_INPUT_DATA!
+    options: String
+    paymentId: ID!
+    productId: [PRODUCT_ORDER]
+    serviceId: ID
+    shippingAddress: SHIPPING_ADDRESS_DATA
+  }
+
+  type Admin {
+    email: String!
+  }
+
   type AuthToken {
     authToken: String!
   }
@@ -79,6 +202,17 @@ const typeDefs = gql`
     startTime: String!
   }
 
+  type ClientSecret {
+    clientSecret: String!
+  }
+
+  type CustomerData {
+    email: String!
+    firstName: String!
+    lastName: String!
+    phoneNumber: String!
+  }
+
   type Product {
     _id: ID!
     available: Boolean!
@@ -90,6 +224,44 @@ const typeDefs = gql`
     options: [String]
     price: Float!
     salePrice: Float
+  }
+
+  type Purchase {
+    _id: ID!
+    comments: String
+    customer: CustomerData!
+    options: String
+    paymentId: ID!
+    productId: [Product]
+    serviceId: Service
+    shippingAddress: ShippingAddress
+  }
+
+  type Service {
+    _id: ID!
+    available: Boolean!
+    creatorId: Admin!
+    description: String!
+    duration: Int!
+    images: [String]
+    lastEditorId: Admin
+    name: String!
+    options: [String]
+    price: Float!
+    salePrice: Float
+  }
+
+  type ShippingAddress {
+    streetName: String!
+    number: Int!
+    suburb: String!
+    postcode: Int!
+    state: String!
+  }
+
+  type SignatureData {
+    signature: String!
+    timestamp: Int!
   }
 `;
 
