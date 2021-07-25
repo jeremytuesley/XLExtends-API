8659480521979ea7a132be0d5fbc3e61a3520af9
thankyouthatscool <85291297+thankyouthatscool@users.noreply.github.com>
2021-07-25 12:42:13 +1000

    Refactored contact resolver.

--- src/graphql/mutations/contact.js
+++ src/graphql/mutations/contact.js
@@ -1,8 +1,10 @@
-// const sgMail = require('@sendgrid/mail');
+const sgMail = require('@sendgrid/mail');
 const fs = require('fs');
 const nodemailer = require('nodemailer');
 const path = require('path');
 const validator = require('validator');
+
+const { v4: uuidv4 } = require('uuid');
 
 const { TooManyAttachments, BadUserInputError } = require('../../errors/CustomErrors');
 const {
@@ -13,7 +15,7 @@ const {
   writeStreamToFile,
 } = require('../../utils/fsPromises');
 
-const contact = async (_, { contactData: { comments, contact, files, name } }) => {
+const contact = async (_, { contactData: { comments, contact, files = [], name } }) => {
   const errors = [];
 
   if (validator.isEmpty(contact)) errors.push({ message: 'Contact details are required.' });
@@ -24,7 +26,7 @@ const contact = async (_, { contactData: { comments, contact, files, name } }) =
 
   const resolvedFiles = await Promise.all(files);
 
-  if (resolvedFiles.length > 5) throw new TooManyAttachments();
+  if (resolvedFiles.length > 1) throw new TooManyAttachments();
 
   try {
     await fsAccessPromise(path.join(__dirname, 'attachments'));
@@ -32,91 +34,94 @@ const contact = async (_, { contactData: { comments, contact, files, name } }) =
     await fsMakeDirPromise(path.join(__dirname, 'attachments'));
   }
 
+  const uniqueFilename = uuidv4();
+
   await Promise.all(
-    resolvedFiles.map(({ createReadStream, filename }) =>
-      writeStreamToFile(createReadStream, path.join(__dirname, 'attachments', filename)),
+    resolvedFiles.map(({ createReadStream }) =>
+      writeStreamToFile(createReadStream, path.join(__dirname, 'attachments', uniqueFilename)),
     ),
   );
 
   const readFiles = await Promise.all(
-    resolvedFiles.map(({ filename }) =>
-      fsReadPromise(path.join(__dirname, 'attachments', filename)),
-    ),
+    resolvedFiles.map(() => fsReadPromise(path.join(__dirname, 'attachments', uniqueFilename))),
   );
 
-  // TODO: Check attachments.
-
-  //   const message = {
-  //     to: 'o.zahnitko@gmail.com',
-  //     from: 'js.t3a2@gmail.com',
-  //     subject: `${name} left you a message.`,
-  //     attachments: resolvedFiles.map(({ filename, mimetype }, index) => ({
-  //       content: readFiles[index].toString('base64'),
-  //       filename,
-  //       type: mimetype,
-  //       disposition: 'attachment',
-  //     })),
-  //     html: `<div>
-  //     <h1>${name} is trying to get in touch.</h1>
-  //     <h2>Comments</h2>
-  //     <div>
-  //     ${comments}
-  //     </div>
-  //     <h2>Contact</h2>
-  //     <div>
-  //     ${contact}
-  // </div>
-  //     </div>`,
-  //   };
-
-  // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-  // try {
-
-  //   await sgMail.send(message);
-  //   return true;
-  // } catch (error) {
-  //   console.log(error);
-  //   return false;
-  // }
-
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-      user: process.env.NODEMAILER_USERNAME,
-      pass: process.env.NODEMAILER_PASSWORD,
-    },
-  });
-
-  try {
-    const emailSentResponse = await transporter.sendMail({
-      from: 'js.t3a2@gmail.com',
-      to: 'o.zahnitko@gmail.com',
-      subject: `Hello from ${name}`,
-      attachments: readFiles.map(({ filename }, index) => ({
-        content: readFiles[index],
-        filename,
-        encoding: 'base64',
-      })),
-      text: 'Hello world.',
-      html: `<div>
-      <h1>${name} is trying to get in touch</h1>
-      <div>${comments}</div>
-      </div>`,
+  if (process.env.NODE_ENV === 'test') {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: process.env.NODEMAILER_USERNAME,
+        pass: process.env.NODEMAILER_PASSWORD,
+      },
     });
 
-    console.log(emailSentResponse);
+    try {
+      await transporter.sendMail({
+        from: 'js.t3a2@gmail.com',
+        to: 'o.zahnitko@gmail.com',
+        subject: `Hello from ${name}`,
+        attachments: readFiles.map(({ filename }, index) => ({
+          content: readFiles[index],
+          filename,
+          encoding: 'base64',
+        })),
+        text: 'Hello world.',
+        html: `<div>
+        <h1>${name} is trying to get in touch</h1>
+        <div>${comments}</div>
+        </div>`,
+      });
+      await Promise.all(
+        resolvedFiles.map(({ filename }) =>
+          fsUnlinkPromise(path.join(__dirname, 'attachments', filename)),
+        ),
+      );
+      fs.rmdir(path.join(__dirname, 'attachments'), () => {});
+      return true;
+    } catch {
+      return false;
+    }
+  } else {
+    const message = {
+      to: process.env.ADMIN_RECIPIENT_EMAIL_ADDRESS,
+      from: process.env.ADMIN_SENDER_EMAIL_ADDRESS,
+      subject: `${name} left you a message.`,
+      attachments: resolvedFiles.map(({ mimetype }, index) => ({
+        content: readFiles[index].toString('base64'),
+        filename: uniqueFilename,
+        type: mimetype,
+        disposition: 'attachment',
+      })),
+      html: `<div>
+      <h1>${name} is trying to get in touch.</h1>
+      <h2>Comments</h2>
+      <div>
+      ${comments || ''}
+      </div>
+      <h2>Contact</h2>
+      <div>
+      ${contact}
+  </div>
+      </div>`,
+    };
 
-    await Promise.all(
-      resolvedFiles.map(({ filename }) =>
-        fsUnlinkPromise(path.join(__dirname, 'attachments', filename)),
-      ),
-    );
-    fs.rmdir(path.join(__dirname, 'attachments'), () => {});
-    return true;
-  } catch {
-    return false;
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    try {
+      await Promise.all([
+        await sgMail.send(message),
+        await Promise.all(
+          resolvedFiles.map(() =>
+            fsUnlinkPromise(path.join(__dirname, 'attachments', uniqueFilename)),
+          ),
+        ),
+      ]);
+      fs.rmdir(path.join(__dirname, 'attachments'), () => {});
+      return true;
+    } catch (error) {
+      return false;
+    }
   }
 };
 
