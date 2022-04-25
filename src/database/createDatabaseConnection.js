aec505341376081b01575c9dfc473ab153706803
jeremytuesley <jeremytuesley@gmail.com>
2022-04-26 02:32:56 +1000

    deployment issue fix

--- src/database/createDatabaseConnection.js
+++ src/database/createDatabaseConnection.js
@@ -4,9 +4,7 @@ const createDatabaseConnection = async () => {
   console.log('Connecting to the database...');
 
   await mongoose.connect(
-    process.env.NODE_ENV === 'dev' && process.env.MONGO_DB === 'local'
-      ? `mongodb+srv://${process.env.DB_ATLAS_USERNAME}:${process.env.DB_ATLAS_PASSWORD}@cluster0.6hzv6.mongodb.net/XLExtends?retryWrites=true&w=majority`
-      : '',
+    `mongodb+srv://${process.env.DB_ATLAS_USERNAME}:${process.env.DB_ATLAS_PASSWORD}@cluster0.6hzv6.mongodb.net/XLExtends?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
